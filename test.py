d064aae7c53bd48104dbb0de43c004aed8b717ac
Thomas Ziereis <ziereis@roofline.ai>
2025-05-04 19:53:22 +0200

    update test script

--- test.py
+++ test.py
@@ -2,21 +2,23 @@ import torch
 import time
 from pathlib import Path
 import numpy
-
-from attic.runtime.runtime import AtticRuntime, AtticRTDriver, AtticFunction
+import iree.runtime as ireert
 
 
 a = torch.rand(16, 16, dtype=torch.float16)
 b = torch.rand(16, 16, dtype=torch.float16)
 
 
-rt = AtticRuntime("tileandfuse_nv_sync.vmfb", driver=AtticRTDriver.CUDA)
+config = ireert.Config("cuda")
+vmm = ireert.VmModule.mmap(config.vm_instance, "matmul.vmfb")
+ctx = ireert.SystemContext(vm_modules=[vmm], config=config)
 # rt = AtticRuntime("contract_nv_sync.vmfb", driver=AtticRTDriver.CUDA)
-func = rt.get_function_by_name("matmul")
+func = ctx.modules.module["matmul"]
+
 
 res = func(a, b)
 torch.set_printoptions(threshold=10_000)
 numpy.set_printoptions(threshold=10_000)
-print(res)
+print(res.to_host())
 
-print(torch.matmul(a.to("cuda"),b.to("cuda")))
+print(torch.matmul(a.to("cuda"), b.to("cuda")))
